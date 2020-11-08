eb66dfb2ff4715e969350eb7e622b78f3ea57e61
othman82 <osmaanly@gmail.com>
2020-11-08 18:50:34 +0000

    sprite

--- Minefield/Form1.cs
+++ Minefield/Form1.cs
@@ -12,9 +12,47 @@ namespace Minefield
 {
     public partial class Form1 : Form
     {
+        int atX = 10;
+        int atY = 20;
+
         public Form1()
         {
             InitializeComponent();
+            //place the sprite at its start-up location
+            drawsprite(atX, atY);
         }
+
+        private void drawsprite (int x, int y)
+        {
+            Label lbl = getLabel(atX, atY);
+            lbl.BackColor = Color.White;
+            lbl.Image = Properties.Resources.Soldier;
+        }
+
+        //Using this it is easy to set up the start position of the sprite on start-up…
+        private Label getLabel(int x, int y)
+        {
+            int k = (y - 1) * 20 + x;
+            string s = "label" + k.ToString();
+
+            foreach (Control c in panel1.Controls)
+            {
+                if (c.GetType()==typeof(System.Windows.Forms.Label))
+                {
+                    if (c.Name==s)
+                    {
+                        return (Label)c;
+                    }
+                }
+            }
+            return null;
+        }
+
+       private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
